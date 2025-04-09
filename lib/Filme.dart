f793713276dd74cac69fc79a33796a44c913858a
Caetano-Ventura-Coutinho <currytano@gmail.com>
2025-04-09 10:45:27 -0300

    arquivo json atualizado e estrutura de classes mudaram

--- lib/Filme.dart
+++ lib/Filme.dart
@@ -1,33 +1,32 @@
-import 'Diretor.dart';
-import 'Genero.dart';
-
 class Filme {
   late String nome;
   late String descricao;
   late String poster;
-  late Diretor diretor;
-  late Genero genero;
+  late String diretor;
+  late String genero;
   Filme()
   {
     nome = '';
     descricao = '';
     poster = '';
+    diretor = '';
+    genero = '';
   }
 
   Filme.v(this.nome, this.descricao, this.poster, this.diretor, this.genero,);
   
   Filme.fromjson(Map<String, dynamic> json)
-  : nome = json['Name'] as String,
-  descricao = json['Description'] as String,
-  poster = json['Poster'] as String,
-  genero = json['Genre'] as Genero,
-  diretor = json['Director'] as Diretor;
+  : nome = json['title'] as String,
+  descricao = json['description'] as String,
+  poster = json['poster'] as String,
+  genero = json['genre'] as String,
+  diretor = json['director'] as String;
 
   Map<String, dynamic> toJson() => {
-  'Name' : nome,
-  'Description' : descricao,
-  'Poster' : poster,
-  'Genre' : genero,
-  'Director': diretor,
+  'title' : nome,
+  'description' : descricao,
+  'poster' : poster,
+  'genre' : genero,
+  'director': diretor,
   };
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'Filme.dart';
 import 'dart:convert';
+import 'package:teste_json/Filme.dart';
 
 
 void main() {
@@ -17,10 +17,10 @@ class MainApp extends StatefulWidget {
 
 class MainAPP extends State<MainApp> {
   List<Filme> filmes = List.empty();
-  int total = 0;
+  int total = 2;
   Future<void> readJson() async{
-    final String response  = await rootBundle.loadString('assets/movies.json');
-    Iterable data = await jsonDecode(response);
+    final String response = await rootBundle.loadString('assets/movies.json');
+      Iterable data = await json.decode(response);
     filmes = List<Filme>.from(data.map((model)=> Filme.fromjson(model)));
     total = filmes.length;
     setState((){
@@ -28,20 +28,49 @@ class MainAPP extends State<MainApp> {
       total;
     });
   }
-
+  
    @override
    initState()  {
     super.initState();
-       readJson();
+    readJson();
    }
-
+  
   @override 
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('$total') ,),
+        body: Scrollbar(
+          child: Center(
+          child: Column(
+          children:<Widget> [
+            Text  (
+              'Nesse arquivo temos $total filmes'
+              ),
+          ListView.builder
+          (itemCount: filmes.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(5),
+            itemBuilder: (BuildContext context, int index){
+              return Card(
+                color: Colors.lightBlue,
+                child: Column(
+                  children: <Widget>[
+                   Text(filmes[index].nome),
+                   Text(filmes[index].descricao),
+                   Text(filmes[index].genero),
+                   Text(filmes[index].diretor),
+                   Image(image: NetworkImage(filmes[index].poster)),
+                  ],  
+                ),
+              );
+            },
+            )
+          ],
+          
+        )  
       )
+    ), 
+      ),
     );
   }
   
