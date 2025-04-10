6031f8c3a3f36c791d1282c38d9684fa568dc7d8
Caetano-Ventura-Coutinho <currytano@gmail.com>
2025-04-10 10:34:55 -0300

    imagens exibidas e scroll funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:teste_json/Filme.dart';
 
-
 void main() {
   runApp(const MainApp());
 }
@@ -17,62 +16,62 @@ class MainApp extends StatefulWidget {
 
 class MainAPP extends State<MainApp> {
   List<Filme> filmes = List.empty();
-  int total = 2;
-  Future<void> readJson() async{
+  int total = 0;
+  Future<void> readJson() async {
     final String response = await rootBundle.loadString('assets/movies.json');
-      Iterable data = await json.decode(response);
-    filmes = List<Filme>.from(data.map((model)=> Filme.fromjson(model)));
+    Iterable data = await json.decode(response);
+    filmes = List<Filme>.from(data.map((model) => Filme.fromjson(model)));
     total = filmes.length;
-    setState((){
+    setState(() {
       filmes;
       total;
     });
   }
-  
-   @override
-   initState()  {
+
+  @override
+  initState() {
     super.initState();
     readJson();
-   }
-  
-  @override 
-  Widget build(BuildContext context){
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Scrollbar(
-          child: Center(
-          child: Column(
-          children:<Widget> [
-            Text  (
-              'Nesse arquivo temos $total filmes'
-              ),
-          ListView.builder
-          (itemCount: filmes.length,
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(5),
-            itemBuilder: (BuildContext context, int index){
-              return Card(
-                color: Colors.lightBlue,
+        body:  Center(
                 child: Column(
-                  children: <Widget>[
-                   Text(filmes[index].nome),
-                   Text(filmes[index].descricao),
-                   Text(filmes[index].genero),
-                   Text(filmes[index].diretor),
-                   Image(image: NetworkImage(filmes[index].poster)),
-                  ],  
-                ),
-              );
-            },
+          children: <Widget>[
+            Text('Nesse arquivo temos $total filmes'),
+            Expanded(
+            child: ListView.builder(
+              itemCount: filmes.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              padding: const EdgeInsets.all(20),
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Colors.lightBlue,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            filmes[index].nome,
+                          ),
+                          Text(filmes[index].descricao),
+                          Text(filmes[index].genero),
+                          Text(filmes[index].diretor),
+                          Image(image: NetworkImage(filmes[index].poster)),
+                        ],
+                      ),
+                    ),
+                  );
+              },
+            ),
             )
           ],
-          
-        )  
-      )
-    ), 
-      ),
-    );
+        ))),
+      );
+    
   }
-  
 }
-
